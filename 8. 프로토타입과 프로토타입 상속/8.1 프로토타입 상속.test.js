88c46965a479af68297bbe9c7c0ae83db1bd29bd
승종 배 <succeeding@nexon.co.kr>
2022-08-09 17:50:37 +0900

    + 8.1 프로토타입 상속

--- 8. 프로토타입과 프로토타입 상속/8.1 프로토타입 상속.test.js
+++ 8. 프로토타입과 프로토타입 상속/8.1 프로토타입 상속.test.js
@@ -1,4 +1,9 @@
+/*
+this를 코드 내에 포함하고 있는 프로토타입의 메서드를 객체에서 호출했든 프로토타입에서 호출했든 상관없이 this는 언제나 .앞에 있는 객체이다.
+  ex) `admin.fullName=`으로 setter 함수를 호출할 때, this는 admin이다.
 
+for...in은 상속 프로퍼티도 순회대상에 포함시킨다.
+*/
 describe('프로토타입 상속', () => {
   let animal = {
     eats: true,
@@ -30,6 +35,69 @@ describe('프로토타입 상속', () => {
     }
 
     rabbit.walk()
+
+
+    let user = {
+      name: 'John',
+      surname: 'Smith',
+
+      set fullName(value) {
+        [this.name, this.surname] = value.split(' ')
+      },
+
+      get fullName() {
+        return `${this.name} ${this.surname}`
+      }
+    }
+
+    let admin = {
+      __proto__: user,
+      isAdmin: true
+    }
+
+    console.log(admin.fullName)
+
+    admin.fullName = 'Alice Copper'
+
+    console.log(admin.fullName)
+    console.log(user.fullName)
   })
-  }
-)
+  test('this가 나타내는 것', () => {
+    let animal = {
+      walk() {
+        if (!this.isSleeping) {
+          console.log('동물어 걸어갑니다.')
+        }
+      },
+      sleep() {
+        this.isSleeping = true
+      }
+    }
+
+    let rabbit = {
+      name: '하얀 토끼',
+      __proto__: animal
+    }
+
+    rabbit.sleep()
+
+    console.log(rabbit.isSleeping)
+    console.log(animal.isSleeping)
+
+  })
+  test('for...in 반복문',() => {
+    console.log(Object.keys(rabbit))
+    // for (let prop in rabbit) console.log(prop)
+
+
+    for (let prop in rabbit) {
+      let isOwn = rabbit.hasOwnProperty(prop)
+
+      if (isOwn) {
+        console.log(`객체 자신의 프로퍼티: ${prop}`)
+      } else {
+        console.log(`상속 프로퍼티: ${prop}`)
+      }
+    }
+  })
+})

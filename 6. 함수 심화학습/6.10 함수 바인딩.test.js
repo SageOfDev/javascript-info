29443c9681a37843c940e1cb6bbacff93d85fd36
sj <bcj0114@gmail.com>
2022-10-16 12:00:05 +0900

    + UPDATE 6.10

--- 6. 함수 심화학습/6.10 함수 바인딩.test.js
+++ 6. 함수 심화학습/6.10 함수 바인딩.test.js
@@ -2,4 +2,21 @@
 setTimeout에 메서드를 전달할 때처럼, 객체 메서드를 콜백으로 전달할 때 ’this 정보가 사라지는’ 문제가 생깁니다.
  */
 
-// 웹 페이지 읽어보는 것으로 충분
+describe('함수 바인딩', () => {
+  /**
+  객체 메서드가 객체 내부가 아닌 다른 곳에 전달되어 호출되면 `this` 가 사라집니다.
+   */
+  test("사라진 'this", async () => {
+    let user = {
+      firstName: 'John',
+      sayHi() {
+        console.log(`Hello, ${this.firstName}!`)
+      }
+    }
+
+    await new Promise(resolve => {
+      setTimeout(user.sayHi, 1000) // undefined
+      resolve()
+    })
+  })
+})

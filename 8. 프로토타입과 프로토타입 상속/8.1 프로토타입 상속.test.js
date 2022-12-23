608342b4852d5e3efdd4ff90f09de316ad077b9a
승종 배 <succeeding@nexon.co.kr>
2022-12-23 16:17:18 +0900

    + UPDATE 8.1. 8.2

--- 8. 프로토타입과 프로토타입 상속/8.1 프로토타입 상속.test.js
+++ 8. 프로토타입과 프로토타입 상속/8.1 프로토타입 상속.test.js
@@ -1,9 +1,5 @@
 /**
-this를 코드 내에 포함하고 있는 프로토타입의 메서드를 객체에서 호출했든 프로토타입에서 호출했든 상관없이 this는 언제나 .앞에 있는 객체이다.
-  ex) `admin.fullName=`으로 setter 함수를 호출할 때, this는 admin이다.
-
-for...in은 상속 프로퍼티도 순회대상에 포함시킨다.
-*/
+ */
 describe('프로토타입 상속', () => {
   let animal = {
     eats: true,
@@ -15,6 +11,21 @@ describe('프로토타입 상속', () => {
     jumps: true,
     __proto__: animal
   }
+  // rabbit.__proto__ = animal 이 코드도 동일함
+  /**
+   * [[Prototype]]
+   * - JS의 모든 객체가 갖는 숨김 프로퍼티
+   * - 값은 null이거나 다른 객체에 대한 참조
+   * - 다른 객체를 참조하는 경우, 참조 대상을 '프로토타입'이라 부름
+   * - 숨김 프로퍼티이나 __proto__ 프로퍼티를 사용하여 값을 설정할 수 있음
+   *
+   * 프로토타입 상속
+   * - 객체에 읽으려는 프로퍼티가 없으면, 프로토타입의 프로퍼티를 찾는 동작 방식
+   *
+   * 프로토타입 체이닝 제약사항
+   * 1. 순환 참조 불가
+   * 2. __proto__의 값은 null이나 객체만 가능하며, 다른 자료형은 무시됨
+   */
   test('[[Prototype]]', () => {
     let longEar = {
       earLength: 10,
@@ -29,6 +40,7 @@ describe('프로토타입 상속', () => {
     longEar.walk()
     console.log(longEar.jumps)
   })
+
   test('프로토타입은 읽기 전용이다', () => {
     rabbit.walk = function() {
       console.log('토끼가 깡충깡충 뜁니다.')
@@ -57,11 +69,17 @@ describe('프로토타입 상속', () => {
 
     console.log(admin.fullName)
 
-    admin.fullName = 'Alice Copper'
+    admin.fullName = 'Alice Copper' // setter 함수가 실행됨
 
     console.log(admin.fullName)
     console.log(user.fullName)
   })
+
+  /**
+   * this는 프로토타입에 영향을 받지 않는다.
+   * - 메서드를 객체에서 호출했든 프로토타입에서 호출했든 상관없이 this는 언제나 .앞에 있는 객체이다.
+   *   ex) `admin.fullName=`으로 setter 함수를 호출할 때, this는 admin이다.
+   */
   test('this가 나타내는 것', () => {
     let animal = {
       walk() {
@@ -85,6 +103,10 @@ describe('프로토타입 상속', () => {
     console.log(animal.isSleeping)
 
   })
+
+  /**
+   * for...in은 상속 프로퍼티도 순회대상에 포함시킨다.
+   */
   test('for...in 반복문',() => {
     console.log(Object.keys(rabbit))
     // for (let prop in rabbit) console.log(prop)
--- 8. 프로토타입과 프로토타입 상속/8.2 함수의 prototype 프로퍼티.test.js
+++ 8. 프로토타입과 프로토타입 상속/8.2 함수의 prototype 프로퍼티.test.js
@@ -1,8 +1,9 @@
-/**
-Rabbit.prototype = animal은 "new Rabbit을 호출해 만든 새로운 객체의 [[Prototype]]을 animal로 설정하라."는 것을 의미합니다.
-
- */
 describe('함수의 prototype 프로퍼티', () => {
+  /**
+   * Rabbit.prototype = animal은 "new Rabbit을 호출해 만든 새로운 객체의 [[Prototype]]을 animal로 설정하라."는 것을 의미합니다.
+   *
+   * F.prototype은 F에 정의된 '일반' 프로퍼티다.
+   */
   test('프로토타입과 프로토타입 상속', () => {
     let animal = {
       eats: true
@@ -12,51 +13,51 @@ describe('함수의 prototype 프로퍼티', () => {
       this.name = name
     }
 
-    Rabbit.prototype = animal
-
     let rabbit = new Rabbit('흰 토끼')
 
     console.log(rabbit.eats)
   })
 
+  /**
+   * 함수는 디폴트로 prototype 프로퍼티를 갖는다.
+   * - 디폴트 prototype 프로퍼티는 constructor 프로퍼티를 갖고 있는 객체를 가리킨다.
+   * - constructor 프로퍼티는 함수 자신을 가리킨다.
+   *
+   * 함수의 prototype 프로퍼티를 조작하지 않고 함수로 객체를 생성하는 경우, 해당 객체는 위의 constructor 프로퍼티를 갖는 객체를 프로토타입으로 한다.
+   * - 따라서 생성된 객체의 constructor 는 생성한 함수를 가리킨다.
+   */
   test('함수의 디폴트 프로퍼티 prototype과 constructor 프로퍼티', () => {
     function Rabbit() {}
-
     console.log(Rabbit.prototype.constructor === Rabbit )
-
-
     let rabbit = new Rabbit()
-
     console.log(rabbit.constructor === Rabbit)
 
 
-    function Rabbit(name) {
+    function Rabbit2(name) {
       this.name = name
       console.log(name)
     }
-
-    rabbit = new Rabbit('흰 토끼')
-
+    rabbit = new Rabbit2('흰 토끼')
     let rabbit2 = new rabbit.constructor('검정 토끼')
-
-
-    Rabbit.prototype.jumps = true
   })
-  test('함수의 디폴트 프로퍼티 prototype과 constructor 프로퍼티', () => {
+
+  test('도중에 F.prototype을 조작하는 경우 constructor 프로퍼티가 보장되지 않는다.', () => {
     function Rabbit() {}
 
-    console.log(Rabbit.prototype.constructor === Rabbit )
-
-
+    Rabbit.prototype = {
+      jumps: true
+    }
     let rabbit = new Rabbit()
-
     console.log(rabbit.constructor === Rabbit)
 
+    // 이 경우 prototype 전체를 덮어쓰지 말고 프로퍼티를 추가 및 제거 하라.
+    function Rabbit2() {}
+    Rabbit2.prototype.jumps = true
+    let rabbit2 = new Rabbit2()
+    console.log(rabbit2.constructor === Rabbit2)
 
-    function Rabbit(name) {
-      this.name = name
-      console.log(name)
-    }
-
+    // 아니면 다시 constructor을 만들어주면 된다.
+    Rabbit.prototype.constructor = Rabbit
+    console.log(rabbit.constructor === Rabbit)
   })
 })
